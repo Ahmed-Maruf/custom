544df1f1524ea8af843eb8a59abcd66762f91d29
Ahmed Maruf <maruf101428@gmail.com>
2019-04-25 02:35:13 +0600

    word-count refactored

--- word-count/word-count.php
+++ word-count/word-count.php
@@ -1,42 +1,31 @@
 <?php
-	/*
-	Plugin Name:  Word Count
-	Plugin URI:   https://developer.wordpress.org/plugins/word-count/
-	Description:  Basic WordPress Plugin for word count in a post
-	Version:      1.0
-	Author:       Ahmed Maruf
-	Author URI:
-	License:      GPL2
-	License URI:  https://www.gnu.org/licenses/gpl-2.0.html
-	Text Domain:  word-count
-	Domain Path:  /languages/
-	*/
-	
-	
-	/*Function to run during the activation of a plugin*/
-	function wordcount_activation_hook(){
-	
-	}register_activation_hook(__FILE__,'wordcount_activation_hook');
-	
-	/*Function to run during the deactivation of a plugin*/
-	function wordcount_deactivation_hook(){
-	
-	}register_deactivation_hook(__FILE__,'wordcount_deactivation_hook');
-	
-	/*For translation hard coded string*/
-	function wordcount_load_textdomain(){
-		load_plugin_textdomain('word-count',false,dirname(__FILE__).'/languages');
-	}add_action('plugins_loaded','wordcount_load_textdomain');
-	
-	/*Filter the content of a post by calling wp api*/
-	function wordcount_count_words($content){
-		
-		$stripped_content = strip_tags($content);
-		$totalWords = str_word_count($stripped_content);
-		$label = __('Total Number of Words','word-count');
-		$label = apply_filters('wordcount_label',$label);
-		$tag = apply_filters('wordcount_tag','h2');
-		$content.= sprintf('<%s>%s: %s</%s>',$tag,$label,$totalWords,$tag);
-		return $content;
-		
-	}add_filter('the_content','wordcount_count_words');
+/**
+ * Plugin Name: Word Count
+ * Plugin URI:  https://example.com/plugins/the-basics/
+ * Description: Basic WordPress Plugin Header Comment
+ * Version:     20160911
+ * Author:      Ahmed Maruf
+ * Author URI:  https://author.example.com/
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wordCount
+ * Domain Path: /languages/
+ */
+
+function wordcount_load_textdomain()
+{
+    load_plugin_textdomain($domain = "word-count", $deprecated = false, $plugin_rel_path = dirname(__FILE__) . "/languages");
+}
+add_action($tag = "plugins_loaded", $function_to_add = "wordcount_load_textdomain");
+
+function wordCount_count_words($content)
+{
+    $strip_content = strip_tags($content);
+    $words = str_word_count($strip_content);
+    $label = __('Total Number of Words', 'wordCount');
+    $label = apply_filters($tag = "wordcount_heading", $value = $label);
+    $tag = apply_filters($tag = "wordcount_tag", $value = 'h2');
+    $content .= sprintf('<%s>%s: %s</%s>', $tag, $label, $words, $tag);
+    return $content;
+}
+add_filter($tag = "the_content", $function_to_add = "wordCount_count_words");

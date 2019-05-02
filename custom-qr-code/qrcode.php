1d91ff2e72cdc461168d05d321f1aa952c0c044f
Ahmed Maruf <maruf.ahmed@sjinnovation.com>
2019-05-02 17:36:00 +0600

    Settings with section added to general

--- custom-qr-code/qrcode.php
+++ custom-qr-code/qrcode.php
@@ -28,10 +28,35 @@ function pqrc_display_qr_code($content){
 	}
 	
 	/*Image Diemention*/
-
-	$image_dimention = apply_filters($tag = 'pqrc_image_dimention', $dimention = '400x400');
+	$height = get_option('pqrc_display_height')?get_option('pqrc_display_height'):180;
+	$width = get_option('pqrc_display_width')?get_option('pqrc_display_width'):180;
+	$image_dimention = apply_filters($tag = 'pqrc_image_dimention', $dimention = '{$height}x{$width}');
 
 	$image_src = sprintf('http://api.qrserver.com/v1/create-qr-code/?color=000000&bgcolor=FFFFFF&data=%s&qzone=1&margin=0&size=%s&ecc=L',$current_post_url,$image_dimention);
 	return $content .= sprintf("<div class='qrcode'><img src = '%s' alt='%s'/></div>",$image_src,$current_post_title);	
 }
 add_filter($tag = 'the_content', $function_to_add = 'pqrc_display_qr_code');
+
+function pqrc_settings_init(){
+
+	add_settings_section($id = 'pqrc_section', $title = __('Post to QR Code'), $callback = 'pqrc_section_callback', $page = 'general');
+	add_settings_field($id = 'pqrc_display_height', $title = __('QR Code Height','custom-qr-code'), $callback = 'pqrc_display_height', $page = 'general', $section = 'pqrc_section');
+	add_settings_field($id = 'pqrc_display_width', $title = __('QR Code Width','custom-qr-code'), $callback = 'pqrc_display_width', $page = 'general', $section = 'pqrc_section');
+
+	register_setting($option_group = 'general', $option_name = 'pqrc_display_height', $sanitize_callback = array('sanitize_callback' => 'esc_attr'));
+	register_setting($option_group = 'general', $option_name = 'pqrc_display_width', $sanitize_callback = array('sanitize_callback' => 'esc_attr'));
+
+}add_action($tag = 'admin_init', $function_to_add = 'pqrc_settings_init');
+
+function pqrc_section_callback(){
+	echo "<p>".__("Settings for Qr Code","post-to-qr-code")."</p>";
+}
+function pqrc_display_height(){
+	$height = get_option($option = 'pqrc_display_height');
+	printf("<input type='text' id='%s' name='%s' value='%s'/>",'pqrc_height','pqrc_display_height',$height);
+}
+
+function pqrc_display_width(){
+	$width = get_option($option = 'pqrc_display_width');
+	printf("<input type='text' id='%s' name='%s' value='%s'/>",'pqrc_width','pqrc_display_width',$width);
+}
